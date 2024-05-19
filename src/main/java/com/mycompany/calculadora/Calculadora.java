e37d069f066447642560987ff1bb1f4bef3773f1
Marcos <Marcos@DESKTOP-KBV0TCR>
2024-05-19 10:22:11 +0200

    Commit de Netbeans

--- src/main/java/com/mycompany/calculadora/Calculadora.java
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
 package com.mycompany.calculadora;
 
 /**
@@ -10,7 +10,11 @@ package com.mycompany.calculadora;
  */
 public class Calculadora {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // TODO code application logic here
     }
+    
 }
